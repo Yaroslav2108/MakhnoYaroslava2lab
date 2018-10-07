8ce7eaae9a1bfba04ddaab756732a4754762b7d4
Yaroslav2108 <yarikyarikavich21@gmail.com>
2018-10-07 15:19:09 +0300

    Test

---
+++ lection 1/Новая папка/first.js
@@ -0,0 +1,18 @@
+
+
+function Green() {
+	document.getElementById("tabl").style.cssText = 'display:block;color:blue;border-radius: 8px; border:5;';
+    document.getElementById("tablet").style.cssText = 'display:none;';
+}
+
+function Reebok() {
+	document.getElementById("tablet").style.cssText = 'display:block;color:blue;border-radius: 8px; border:5;';
+	document.getElementById("tabl").style.cssText = 'display:none;';
+}
+
+
+
+
+
+
+
